c81b08092d67ce53b71342e3f8172abd3b246f38
Chaitanya Jaiswal <21bch073@nith.ac.in>
2023-05-06 21:40:06 +0530

    Add Q05_3

--- q05.rb
+++ q05.rb
@@ -1,27 +1,13 @@
-cnt = 0
-(0..2).each{|coin500|        # Upto 2 500 coins
-  (0..10).each{|coin100|     # Upto 10 100 coins
-    (0..15).each{|coin50|    # Upto 15 50 coins
-      (0..15).each{|coin10|  # Upto 15 10 coins
-        if coin500 + coin100 + coin50 + coin10 <= 15 then
-          if coin500 * 500 + coin100 * 100 +
-             coin50 * 50 + coin10 * 10 == 1000 then
-            cnt += 1
-          end
-        end
-      }
+@cnt = 0
+def change(target, coins, usable)
+  coin = coins.shift
+  if coins.size == 0 then
+    @cnt += 1 if target / coin <= usable
+  else
+    (0..target/coin).each{|i|
+      change(target - coin * i, coins.clone, usable - i)
     }
-  }
-}
-puts cnt
-
-# Second Part of Question
-
-coins = [10, 50, 100, 500]
-cnt = 0
-(2..15).each do |i|
-  coins.repeated_combination(i).each{|coin_set|
-    cnt += 1 if coin_set.inject(:+) == 1000
-  }
+  end
 end
-puts cnt
+change(1000, [500, 100, 50, 10], 15)
+puts @cnt
